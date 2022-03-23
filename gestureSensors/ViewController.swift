c169610b4df39f371cee87d2a13883666c852230
Tuba Nur <yasatubanur@gmail.com>
2022-03-23 22:13:46 +0300

    image and label changed with UITapGestureRecognizer

--- gestureSensors/ViewController.swift
+++ gestureSensors/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var label: UILabel!
+    
+    
+        var ankara = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changeImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
     }
-
+    @objc func changeImage() {
+        if ankara == false {
+            imageView.image = UIImage(named: "ankara")
+            label.text = "Ankara"
+            ankara = true
+        } else {
+            imageView.image = UIImage(named: "istanbul")
+            label.text = "İstanbul"
+            ankara = false
+        }
+    }
 
 }
 
